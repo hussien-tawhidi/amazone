46ab353213bd63c2d7b6ff9955ca12d9222ecb26
hussien tawhidi <hussientawhidi710@gmail.com>
2023-09-11 10:16:13 +0330

    store

--- src/redux/CartProvider.tsx
+++ src/redux/CartProvider.tsx
@@ -1,11 +1,9 @@
-"use client";
 
 import { ReactNode } from "react";
 import { Provider } from "react-redux";
 import { persistor, store } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
 
-interface Props {}
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
   return (
